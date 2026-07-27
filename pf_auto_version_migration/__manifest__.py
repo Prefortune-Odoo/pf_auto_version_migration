3bba3bbcf5e2299eff0152b217b540adbbf265a5
Prefortune-Odoo <153276923+Prefortune-Odoo@users.noreply.github.com>
2026-07-27 16:34:27 +0530

    Removed manifest - Bug Fixing
    Removed post_init_hook from the manifest.

--- pf_auto_version_migration/__manifest__.py
+++ pf_auto_version_migration/__manifest__.py
@@ -24,7 +24,6 @@
         'views/migration_ticket_views.xml',
     ],
     "images": ["static/description/banner.gif"],
-    'post_init_hook': 'post_init_hook',
     'installable' : True,
     'application': True,
     'auto_install' : False,
